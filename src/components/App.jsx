e31a9fab4b9eba8d3961de5b9b571d14292525f0
Nastasiia03 <110775841+Nastasiia03@users.noreply.github.com>
2023-02-28 10:25:47 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,62 @@
+import { ContactForm } from "./ContactForm/ContactForm";
+import { Component } from "react";
+import { ContactList } from "./ContactList/ContactList";
+import { Filter } from "./Filter/Filter";
+import { GlobalStyle } from "./GlobalStyles";
+import { Layout } from "./Layout";
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: "",
+  }
+
+  addContact = (newContact) => {
+    const sameName = this.state.contacts.map(contact => contact.name).includes(newContact.name);
+
+    if (sameName) {
+      alert(`${newContact.name} is already in contacts`)
+    } else {
+      this.setState(prevState => {
+        return {
+          contacts: [...prevState.contacts, newContact],
+        }
+      })
+    };
+  }
+
+  filterContact = (filter) => {
+this.setState({filter})
+  }
+
+  findContact = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter((contacts) =>
+      contacts.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  deleteContact = (contactId) => {
+    this.setState(prevState => {
+      return {
+    contacts: prevState.contacts.filter(contact => contact.id !== contactId)
+  }
+})
+  }
+  
+  render() {
+    const findedContacts = this.findContact();
+    return (
+      <Layout>
+        <GlobalStyle/>
+        <h1>Phonebook</h1>
+        <ContactForm onSave={this.addContact} />
+
+        <h2>Contacts</h2>
+          <Filter value={this.state.filter} onFind={this.filterContact} />
+        <ContactList contacts={findedContacts} onDelete={this.deleteContact} />
+
+      </Layout>
+    )
+  }
+}
